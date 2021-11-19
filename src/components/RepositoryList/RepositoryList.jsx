161d393744d7a58a9e06e0f33a52b3d3ff879d18
jerejs <jere.salmensaari@metropolia.fi>
2021-11-19 15:04:46 +0200

    Added 10.24

--- src/components/RepositoryList/RepositoryList.jsx
+++ src/components/RepositoryList/RepositoryList.jsx
@@ -1,16 +1,18 @@
-import React, { useState, useRef } from 'react';
-import { FlatList, View, StyleSheet } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { FlatList, View, StyleSheet, TouchableOpacity } from 'react-native';
+import { useDebounce } from 'use-debounce';
 import { Picker } from 'react-native';
 import theme from '../../theme';
 import RepositoryListItem from './RepositoryListItem';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
-import { faCaretDown } from '@fortawesome/free-solid-svg-icons';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
 import Text from '../Text';
 
 import { useHistory } from 'react-router-native';
 import useRepositories from '../../hooks/useRepositories';
 import { setIn } from 'formik';
+import { TextInput } from '../TextInput';
 
 const styles = StyleSheet.create({
   mainBack: {
@@ -20,14 +22,29 @@ const styles = StyleSheet.create({
 		height: 10,
 		backgroundColor: theme.colors.separatorBack,
 	},
-  sortMenuWrapper: {
+  SearchMenuWrapper: {
     display: 'flex',
     flexGrow: 0,
     justifyContent: 'center',
-    backgroundColor: theme.colors.cardBack,
+    backgroundColor: theme.colors.separatorBack,
     margin: 10,
   },
-  sortMenuButtonWrapper: {
+  searchMenuTextInputWrapper: {
+    display: 'flex',
+    flexDirection: 'row',
+
+    backgroundColor: theme.colors.cardBack,
+    paddingTop: 5,
+    paddingBottom: 5,
+    paddingLeft: 10,
+    paddingRight: 10,
+    margin: 5,
+    borderRadius: 2,
+  },
+  searchMenuTextInput: {
+    width: '92%'
+  },
+  SearchMenuButtonWrapper: {
     display: 'flex',
     flexDirection: 'row',
     justifyContent: 'space-between',
@@ -35,13 +52,18 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     padding: 20,
   },
+  clear: {
+    alignSelf: 'center',
+    padding: 5,
+  }
 });
 
 export const ItemSeparator = () => <View style={styles.separator} />;
 
 
 
-export const RepositoryListContainer = ({ repositories, history, SortMenu}) => {
+export const RepositoryListContainer = ({ repositories, setFilter, setSort}) => {
+  const history = useHistory();
 	const repositoryNodes = repositories
 		? repositories.edges.map(edge => edge.node)
     : [];
@@ -49,7 +71,7 @@ export const RepositoryListContainer = ({ repositories, history, SortMenu}) => {
 	return (
     <View style={styles.mainBack}>
 		  <FlatList
-        ListHeaderComponent={SortMenu}
+        ListHeaderComponent={<SearchMenu setFilter={setFilter} setSort={setSort} />}
 		  	style={{ height: '85%' }}
 		  	data={repositoryNodes}
 		  	ItemSeparatorComponent={ItemSeparator}
@@ -61,36 +83,61 @@ export const RepositoryListContainer = ({ repositories, history, SortMenu}) => {
 	);
 };
 
+const SearchMenu = ({ setFilter, setSort }) => {
+  const [value, setValue] = useState('CREATED_AT:DESC');
+  const [text, setText] = useState('');
+  const [debounced] = useDebounce(text, 500);
+
+  useEffect(() => {
+    setFilter(debounced)
+  }, [debounced])
+
+  return (
+    <View style={styles.SearchMenuWrapper}>
+      <View style={styles.searchMenuTextInputWrapper}>
+        <TextInput 
+          value={text}
+          placeholder='Filter...'
+          style={styles.searchMenuTextInput}
+          onChangeText={setText}
+        />
+        <TouchableOpacity 
+          style={styles.clear}
+          onPress={() => setText('')}
+        >
+          <FontAwesomeIcon icon={faTimes} />
+        </TouchableOpacity>
+      </View>
+      <Picker
+        selectedValue={value}
+        onValueChange={(itemValue) => {
+          setSort(itemValue);
+          setValue(itemValue);
+        }
+        }
+        mode={'dialog'}
+        prompt='Select an item...'
+      >
+        <Picker.Item label='Latest repositories' value={'CREATED_AT:DESC'} />
+        <Picker.Item label='Highest rated repositories' value={'RATING_AVERAGE:DESC'} />
+        <Picker.Item label='Lowest rated repositories' value={'RATING_AVERAGE:ASC'} />
+      </Picker>
+    </View>
+  )
+}
+
 const RepositoryList= () => {
   const [sort, setSort] = useState('CREATED_AT:DESC');
-
+  const [filter, setFilter] = useState('');
+  
   const sortMethod = sort.split(':')
 	const { repositories } = useRepositories({
     orderBy: sortMethod[0],
-    orderDirection: sortMethod[1]
+    orderDirection: sortMethod[1],
+    searchKeyword: filter
   });
-	const history = useHistory();
 
-  const SortMenu = () => {
-    return (
-      <View style={styles.sortMenuWrapper}>
-        <Picker
-          selectedValue={sort}
-          onValueChange={(itemValue, itemIndex) =>
-            setSort(itemValue)
-          }
-          mode={'dialog'}
-          prompt='Select an item...'
-        >
-          <Picker.Item label='Latest repositories' value={'CREATED_AT:DESC'} />
-          <Picker.Item label='Highest rated repositories' value={'RATING_AVERAGE:DESC'} />
-          <Picker.Item label='Lowest rated repositories' value={'RATING_AVERAGE:ASC'} />
-        </Picker>
-      </View>
-    )
-  }
-
-	return <RepositoryListContainer repositories={repositories} history={history} SortMenu={SortMenu} />;
+	return <RepositoryListContainer repositories={repositories} setFilter={setFilter} setSort={setSort} />;
 };
 
 export default RepositoryList;
--- src/hooks/useRepositories.js
+++ src/hooks/useRepositories.js
@@ -2,12 +2,13 @@ import { useQuery } from '@apollo/client';
 
 import { GET_REPOSITORIES } from '../graphql/queries';
 
-const useRepositories = ({ orderBy, orderDirection }) => {
+const useRepositories = ({ orderBy, orderDirection, searchKeyword }) => {
 	const { data, error, loading } = useQuery(GET_REPOSITORIES, {
 		fetchPolicy: 'cache-and-network',
 		variables: {
 			orderBy,
-			orderDirection
+			orderDirection,
+			searchKeyword
 		}
 	});
 	return { repositories: loading ? null : data.repositories, loading, error };
